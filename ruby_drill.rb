d7dc510291bde864bc15df4122308f23c070f1df
soushi0126 <sousi0126@gmail.com>
2024-02-10 15:40:39 +0900

    解答

--- ruby_drill.rb
+++ ruby_drill.rb
@@ -1,13 +1,11 @@
 #配列内の要素である数値を、小さい順に並び替えて出力するプログラム
 
 def bubble_sort(data)
-  length = data.length #配列の数を数える処理を記述
-  
-  # 先頭から隣の数同士の大きさを比べる
-  for i in 0..(length - 1) do
-    for j in 1..(length - 1 - i)
-      if data[j-1] > data[j]
-        data[j-1], data[j] = data[j], data[j-1]
+  length = data.length 
+  for i in 0..(length-1) 
+    for j in 1.. (length-i-1) 
+      if data[j-1] > data[j] 
+        data[j-1],data[j] = data[j],data[j-1] 
       end
     end
   end
