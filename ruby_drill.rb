6d98bc94d3e9cea22a93eb66aedc2e74f700a93d
soushi0126 <sousi0126@gmail.com>
2024-02-10 16:00:54 +0900

    AIに可読性の高いコードに変換

--- ruby_drill.rb
+++ ruby_drill.rb
@@ -1,17 +1,18 @@
 #配列内の要素である数値を、小さい順に並び替えて出力するプログラム
 
 def bubble_sort(data)
-  length = data.length 
-  for i in 0..(length-1) 
-    for j in 1.. (length-i-1) 
-      if data[j-1] > data[j] 
-        data[j-1],data[j] = data[j],data[j-1] 
+  length = data.length
+
+  (0..length - 1).each do |i|
+    (1..length - 1 - i).each do |j|
+      if data[j - 1] > data[j]
+        data[j - 1], data[j] = data[j], data[j - 1]
       end
     end
   end
 end
 
 # 呼び出し
-number = [1,23,4,100,6,12,2,45,79]
-bubble_sort(number)
-puts number
+numbers = [1, 23, 4, 100, 6, 12, 2, 45, 79]
+bubble_sort(numbers)
+puts numbers
