7b74949507c9d950dcfb82ac59b1d8e5511bc95a
soushi0126 <sousi0126@gmail.com>
2024-02-11 11:05:50 +0900

    自分の解答

--- ruby_drill.rb
+++ ruby_drill.rb
@@ -1,18 +1,17 @@
-#配列内の要素である数値を、小さい順に並び替えて出力するプログラム
+# 配列ないの要素の数値を、大きい順に並び替えて出力する
 
 def bubble_sort(data)
-  length = data.length
+  length = data.length #配列内の数を数える処理
 
-  (0..length - 1).each do |i|
-    (1..length - 1 - i).each do |j|
-      if data[j - 1] > data[j]
-        data[j - 1], data[j] = data[j], data[j - 1]
+  for i in 0..(length-1)
+    for j in 1..(length-1-i)
+      if data[j-1] < data[j]
+        data[j-1], data[j] = data[j], data[j-1]
       end
     end
   end
 end
 
-# 呼び出し
-numbers = [1, 23, 4, 100, 6, 12, 2, 45, 79]
+numbers = [1,23,100,3,65,45,8,44,76]
 bubble_sort(numbers)
 puts numbers
